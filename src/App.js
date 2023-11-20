9e04f3031a24c1e95383ff1de8d37324bed577ca
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2023-11-20 09:40:41 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      oi
+    </div>
+  );
+}
+
+export default App;
+// criar a pokedex
+// criar uma function pra realizar o fetch com async await e try catch
+// jogar num loop pra pegar os poekmons
+// jogar com a img
---
+++ src/components/pokedex/Pokedex.jsx
@@ -0,0 +1,11 @@
+import 'pokedex.css'
+
+const Pokedex = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Pokedex;
